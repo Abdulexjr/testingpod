c60c41c70867ab3e240bcb1772631e71566f60ab
Abdulexjr <41876066+Abdulexjr@users.noreply.github.com>
2019-12-19 13:04:08 +0500

    adding Added test cases

---
+++ TestPodCreation/TestingFile.swift
@@ -0,0 +1,23 @@
+//
+//  TestingFile.swift
+//  TestPodCreation
+//
+//  Created by Abdul on 19/12/2019.
+//  Copyright © 2019 Abdul. All rights reserved.
+//
+
+
+
+public final class TestPodCreation {
+
+    let name = "TestPodCreation"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
